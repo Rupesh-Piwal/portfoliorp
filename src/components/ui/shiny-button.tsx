f310480284859824fa105cb481ea5ced2555d25a
rupesh <rpiwal02@gmail.com>
2025-06-21 13:34:00 +0530

    chores: cv updated

--- src/components/ui/shiny-button.tsx
+++ src/components/ui/shiny-button.tsx
@@ -46,7 +46,7 @@ const ShinyButton = React.forwardRef<HTMLButtonElement, ShinyButtonProps>(
           className
         )}
       >
-        <a href="https://drive.google.com/file/d/1X6lB3Mkis3CPigI0Avsf5QDsyIVgjppd/view?usp=sharing">
+        <a href="https://drive.google.com/file/d/1v4zRchh4DulXjwUkP3Ih_dDctJcg3hsg/view?usp=sharing">
           <span
             className="relative block size-full text-sm uppercase tracking-wide text-[rgb(0,0,0,65%)] dark:font-light dark:text-[rgb(255,255,255,90%)]"
             style={{
