99c33d2cc9b046ebb6fa3ee72c1007d549fd4a12
rupesh <rpiwal02@gmail.com>
2026-01-08 22:46:27 +0530

    updated footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -208,8 +208,8 @@ export default function Page() {
                 >
                   with a direct question on twitter
                 </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                {/* and I&apos;ll respond whenever I can. I will ignore all
+                soliciting. */}
               </p>
             </div>
           </BlurFade>
